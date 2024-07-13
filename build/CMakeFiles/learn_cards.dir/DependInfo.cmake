4a1ba41137760998cc80060a43aa80ad4ab18030
David Pavlicek <a12223493@unet.univie.ac.at>
2024-07-13 19:41:30 +0200

    Going from Makefile to CMakelists.txt

---
+++ build/CMakeFiles/learn_cards.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/davidpavlicek03/Desktop/Other/Projects/learn_cards/src/app/card.cpp" "CMakeFiles/learn_cards.dir/src/app/card.cpp.o" "gcc" "CMakeFiles/learn_cards.dir/src/app/card.cpp.o.d"
+  "/Users/davidpavlicek03/Desktop/Other/Projects/learn_cards/src/app/set.cpp" "CMakeFiles/learn_cards.dir/src/app/set.cpp.o" "gcc" "CMakeFiles/learn_cards.dir/src/app/set.cpp.o.d"
+  "/Users/davidpavlicek03/Desktop/Other/Projects/learn_cards/src/main.cpp" "CMakeFiles/learn_cards.dir/src/main.cpp.o" "gcc" "CMakeFiles/learn_cards.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
